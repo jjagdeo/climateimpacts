25387ca87bb1a07eb8881a9dfa86218590d5ce31
jbalmagia <jbalmagia@gmail.com>
2020-03-09 19:33:55 -0700

    watershedstorage() function with documentation

--- R/WatershedStorage.R
+++ R/WatershedStorage.R
@@ -1,8 +1,15 @@
-# Function describing water stored in a watershed/year using simplified inflow/outflow processes
-# Function assumptions: The user must provide watershed_size in square miles and precipitation, evaporation, and runoff in inches per year
+#' Function describing water stored in a watershed/year using simplified inflow/outflow processes
+#'
+#'
+#' @param precip precipitation in inches/year
+#' @param evap evaporation in inches/year
+#' @param runoff runoff in inches/year
+#' @param watershed_size total area of watershed in sq miles
+#' @return storage the amount of water stored in the watershed in cubic ft/year
 
-waterstorage = function(precip, evap, runoff, watershed_size) {
-  storage = ((precip - (evap + runoff)) * 0.0833333) * (watershed_size * 27880000) 
+
+WatershedStorage = function(precip, evap, runoff, watershed_size) {
+  storage = ((precip - (evap + runoff)) * 0.0833333) * (watershed_size * 27880000)
   # Multiply by watershed size to get volumetric storage from rate inputs: precip, evap, runoff
   # Multiple watershed_size (given by user in square miles) by 27,880,000 to convert to square feet
   # Multiply precip - (evap + runoff) (given by user in inches) by 0.0833333 to convert to feet
