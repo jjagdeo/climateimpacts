0ee7111d91d922a38027c8b209fb8cb6f191e9a2
Jessica Jagdeo <jessica32703@gmail.com>
2020-03-12 13:35:59 -0700

    Updated MaxPrecip data and documentation. Still need to fix function

--- R/precip.R
+++ R/precip.R
@@ -4,7 +4,9 @@
 #'
 #'@format A data frame with 365 rows and 2 columns
 #'\describe{
-#'\item{rain}-{Generated precipitation values in inches per day}
 #'\item{date}-{Generated dates from January 1, 1995 to December 31, 1995}
+#'\item{station}-{Rainfall station (Cachuma, Gibraltar, Jameson, or San Marcos)}
+#'\item{rainfall}-{Generated precipitation values in inches per day}
+#'
 #'}
 "precip"
