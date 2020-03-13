b2c7c72d77baac8e31954c7fec852e084dfb793d
jbalmagia <jbalmagia@gmail.com>
2020-03-12 18:10:24 -0700

    finishing touches

--- R/WatershedStorage.R
+++ R/WatershedStorage.R
@@ -15,9 +15,6 @@ WatershedStorage = function(precip, evap, runoff, watershed_size) {
   # Multiply by watershed size to get volumetric storage from rate inputs: precip, evap, runoff
   # Multiple watershed_size (given by user in square miles) by 27,880,000 to convert to square feet
   # Multiply precip - (evap + runoff) (given by user in inches) by 0.0833333 to convert to feet
-  precip = ifelse((precip < 0), return("Precipitation must be greater than or equal to zero"), precip)
-  evap = ifelse((evap < 0), return("Evaporation cannot be negative"), evap)
-  runoff = ifelse((runoff < 0), return("Runoff cannot be negative"), runoff)
   return(storage)
   # Storage is returned in units of cubic feet per year
 }
