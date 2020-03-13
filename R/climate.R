6aca2f4fb1de2e7a996afa9eabaac9a2e2185e48
jbalmagia <jbalmagia@gmail.com>
2020-03-12 18:03:01 -0700

    documentation for all!

---
+++ R/climate.R
@@ -0,0 +1,11 @@
+#'Snow_v_rain function
+#'
+#'Generated data
+#'
+#'@format a data frame with 365 rows and 2 columns
+#'\describe{
+#'\item{precip} - {generated precipitation (inches) for each event}
+#'\item{temp} - {generated temperatures (F) corresponding to each precipitaiton event }
+#'
+#'}
+ "climate"
--- R/snow_v_rain.R
+++ R/snow_v_rain.R
@@ -1,12 +1,16 @@
-#How much snow vs rain
+#'Volume of snow vs rain (inches)
+#'
+#'Takes precipitation (inches) and corresponding temperature (F) data and calculates the volume of precipitation that fell as rain vs snow
+#'
+#'@param climate data frame including precipitation (inches) and temperature (F) observations
+#'@return precip_summary a table with volume of precipitation that fell as rain vs snow and average temperature when precipitation fell as rain vs snow.
 
-#if temp is greater than 32, but precip gets added to rain total.
 
 snow_v_rain = function(climate){
   library(tidyverse)
-  precip_summary<- climate %>%
-    mutate(precip_type = case_when(temp > 32 == "Rain",
-                                   temp < 32 == "Snow")) %>%
+  precip_summary = climate %>%
+    mutate(precip_type = case_when(temp > 32 ~ "Rain",
+                                   temp < 32 ~ "Snow")) %>%
              group_by(precip_type) %>%
              summarise(volume = sum(precip),
                        avg_temp = mean(temp))
