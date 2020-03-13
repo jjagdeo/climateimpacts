e9e297beaa8ee1d68e7701d3c3e81ab8c6d4761d
Jessica Jagdeo <jessica32703@gmail.com>
2020-03-12 18:04:51 -0700

    Changed MeanPrecip() documentation

--- R/MeanPrecip.R
+++ R/MeanPrecip.R
@@ -2,9 +2,8 @@
 #'
 #'
 #' @param precip precipitation in inches/day
-#' @param station describes the number or name of the precipitation gauge or station
+#' @param station describes the name of the precipitation gauge or station
 #' @return the average of the recorded rainfall values
-#' @return the data of the greatest recorded rainfall value
 #' @return the selected rainfall station
 #'
 
@@ -12,8 +11,10 @@ MeanPrecip = function(precip, station = "Cachuma") {
   library(tidyverse)
 
   precip_subset <- precip %>%
-    dplyr::mutate(year_month_day = lubridate::parse_date_time(date, "ymd"),
-                  rainfall = as.numeric(rainfall))
+    dplyr::mutate(year_month_day = lubridate::parse_date_time(date, "ymd"), # Convert date column to R recognized dates
+                  rainfall = as.numeric(rainfall)) # Convert precip column to numeric
+
+  # Use if statement to filter based on user-inputed station (4 choices: Cachuma, Gibraltar, Jameson, and San Marcos)
 
   if (station == "Cachuma")
     precip_subset <- precip_subset %>%
@@ -35,9 +36,11 @@ MeanPrecip = function(precip, station = "Cachuma") {
       dplyr::filter(station == "San Marcos") %>%
       dplyr::select(rainfall)
 
+  # Apply mean function to station's precipitation subset
+
   precip_subset_mean <- apply(precip_subset, MARGIN = 2, FUN = mean)
 
-return(list(Date = date, Station = station, Average_Precip = precip_subset_mean))
+return(list(Station = station, Average_Precip = precip_subset_mean))
 
 }
 
